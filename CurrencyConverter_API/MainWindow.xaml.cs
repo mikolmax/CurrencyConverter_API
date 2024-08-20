a9fecbbe26e05d8e740b7b86c074864d499c21f5
alfon <alfon@MIKOL-DESKTOP>
2024-08-21 01:27:03 +0800

    Add project files.

---
+++ CurrencyConverter_API/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace CurrencyConverter_API
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Root val = new Root();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ClearControls();
+            GetValue();
+        }
+
+        private async void GetValue()
+        {
+            val = await GetData<Root>("https://openexchangerates.org/api/latest.json?app_id=dbb1b7e6b3a2475ca57f3592abcd41ad");
+            BindCurrency();
+        }
+
+        public static async Task<Root> GetData<T>(string url)
+        {
+            var myRoot = new Root();
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.Timeout = TimeSpan.FromMinutes(1);
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var ResponseString = await response.Content.ReadAsStringAsync();
+                        var ResponseObject = JsonConvert.DeserializeObject<Root>(ResponseString);
+
+                        //MessageBox.Show("Timestamp: " + ResponseObject.timestamp, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        return ResponseObject;
+                    }
+                    return myRoot;
+                }
+            }
+            catch (Exception)
+            {
+                return myRoot;
+            }
+        }
+
+        private void BindCurrency()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Text");
+
+            dt.Columns.Add("Value");
+
+            dt.Rows.Add("--SELECT--", 0);
+            dt.Rows.Add("USD", val.rates.USD);
+            dt.Rows.Add("PHP", val.rates.PHP);
+            dt.Rows.Add("BRL", val.rates.BRL);
+            dt.Rows.Add("CAD", val.rates.CAD);
+            dt.Rows.Add("SGD", val.rates.SGD);
+            dt.Rows.Add("GBP", val.rates.GBP);
+            dt.Rows.Add("EUR", val.rates.EUR);
+            dt.Rows.Add("HKD", val.rates.HKD);
+            dt.Rows.Add("JPY", val.rates.JPY);
+            dt.Rows.Add("KRW", val.rates.KRW);
+
+            cmbFromCurrency.ItemsSource = dt.DefaultView;
+
+            cmbFromCurrency.DisplayMemberPath = "Text";
+
+            cmbFromCurrency.SelectedValuePath = "Value";
+
+            cmbFromCurrency.SelectedIndex = 0;
+
+            cmbToCurrency.ItemsSource = dt.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
+        }
+
+        private void ClearControls()
+        {
+            txtCurrency.Text = string.Empty;
+            if (cmbFromCurrency.Items.Count > 0)
+                cmbFromCurrency.SelectedIndex = 0;
+            if (cmbToCurrency.Items.Count > 0)
+                cmbToCurrency.SelectedIndex = 0;
+            lblCurrency.Content = "";
+            txtCurrency.Focus();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("^[0-9]+");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+
+        private void cmbFromCurrency_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbToCurrency_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            double ConvertedValue;
+
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                MessageBox.Show("Please Enter Currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                txtCurrency.Focus();
+                return;
+            }
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency From", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbFromCurrency.Focus();
+                return;
+            }
+            else if (cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency To", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbToCurrency.Focus();
+                return;
+            }
+
+            if (cmbFromCurrency.Text == cmbToCurrency.Text)
+            {
+                ConvertedValue = double.Parse(txtCurrency.Text);
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
+            else
+            {
+                ConvertedValue = (double.Parse(cmbToCurrency.SelectedValue.ToString()) * double.Parse(txtCurrency.Text)) / double.Parse(cmbFromCurrency.SelectedValue.ToString());
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearControls();
+        }
+    }
+}
---
+++ CurrencyConverter_API/Rate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter_API
+{
+    public class Rate
+    {
+        public double USD { get; set; }
+        public double PHP { get; set; }
+        public double BRL { get; set; }
+        public double CAD { get; set; }
+        public double SGD { get; set; }
+        public double GBP { get; set; }
+        public double EUR { get; set; }
+        public double HKD { get; set; }
+        public double JPY { get; set; }
+        public double KRW { get; set; }
+    }
+}
